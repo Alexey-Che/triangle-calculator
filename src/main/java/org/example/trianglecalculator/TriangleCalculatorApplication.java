46fa5efaf20f9a86162c7f64bb1870ab0ac02f99
Aleksei Korotkikh <lexa.36484@gmail.com>
2024-02-17 14:56:56 +0500

    добавил информацию о сервисе

--- src/main/java/org/example/trianglecalculator/TriangleCalculatorApplication.java
+++ src/main/java/org/example/trianglecalculator/TriangleCalculatorApplication.java
@@ -1,8 +1,16 @@
 package org.example.trianglecalculator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Сервис расчета параметров треугольника",
+        version = "0.0.1",
+        contact = @Contact(email = "lexa.36484@gmail.com",
+                url = "https://t.me/KorotkihA"),
+        description = "Коротких А.И. контакты:"))
 @SpringBootApplication
 public class TriangleCalculatorApplication {
 
